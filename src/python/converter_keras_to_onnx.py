a53835a934e3f88f93c6b5ad6089c30a0fa62b4e
sergioprates <sergioprates.student@gmail.com>
2019-10-31 00:23:58 -0300

    ajustado código de conversão onnx

--- src/python/converter_keras_to_onnx.py
+++ src/python/converter_keras_to_onnx.py
@@ -2,13 +2,15 @@ import onnx
 from keras.models import load_model
 import onnxmltools
 import keras2onnx
+import tensorflow as tf
+print(tf.version.VERSION)
 
 
 # carregando nosso modelo do keras
-model = load_model('c:\fer2013_mini_XCEPTION.107-0.66.hdf5')
+model = load_model('./modelo/fer2013_mini_XCEPTION.107-0.66.hdf5')
 
 # convertendo para onnx
 onnx_model = onnxmltools.convert_keras(model, target_opset=7)
 
 # salvando em um arquivo
-onnx.save(onnx_model, 'modelo_analise_sentimento.onnx')
+onnx.save(onnx_model, './modelo/modelo_analise_sentimento.onnx')
