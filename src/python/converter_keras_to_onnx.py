36b4438350fb8b1c8901b54db454744b71d1955c
sergioprates <sergioprates.student@gmail.com>
2019-10-31 00:09:49 -0300

    removido winmltools

--- src/python/converter_keras_to_onnx.py
+++ src/python/converter_keras_to_onnx.py
@@ -1,12 +1,14 @@
 import onnx
 from keras.models import load_model
 import onnxmltools
-import winmltools
 import keras2onnx
 
 
+# carregando nosso modelo do keras
 model = load_model('c:\fer2013_mini_XCEPTION.107-0.66.hdf5')
 
+# convertendo para onnx
 onnx_model = onnxmltools.convert_keras(model, target_opset=7)
 
+# salvando em um arquivo
 onnx.save(onnx_model, 'modelo_analise_sentimento.onnx')
